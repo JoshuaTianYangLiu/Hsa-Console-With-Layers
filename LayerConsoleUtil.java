4ed1de66c4ce339f12e5c0d8aec986ab2a5e9f5b
JoshuaTianYangLiu <46253730+JoshuaTianYangLiu@users.noreply.github.com>
2019-10-20 01:27:16 -0400

    Removed example code in util file

--- LayerConsoleUtil.java
+++ LayerConsoleUtil.java
@@ -1,13 +1,11 @@
 // The "LayerConsole" class.
 import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import hsa.Console;
 
 public class LayerConsoleUtil
 {
     BufferedImage b;
-    Graphics2D g;   //Temp
     Color c=new Color(0,0,0,0);
     LayerConsoleUtil(){
 	b = new BufferedImage(640, 500, BufferedImage.TYPE_INT_ARGB);
@@ -31,18 +29,5 @@ public class LayerConsoleUtil
     }
     void setColor(Color c){
 	this.c=c;
-    }
-    public void draw(){
-	BufferedImage buffer = new BufferedImage(640, 500, BufferedImage.TYPE_INT_ARGB);
-	g = buffer.createGraphics();
-	g.setColor(new Color(0,0,0,0));
-	g.fillRect(0, 0, 640, 500);
-	g.setColor(Color.red);
-	g.fillRect(100,100,300,300);
-	g.finalize();
-	g = buffer.createGraphics();
-	g.setColor(Color.blue);
-	g.fillRect(200,300,400,500);
-	g.finalize();
     }
 } // LayerConsole class
